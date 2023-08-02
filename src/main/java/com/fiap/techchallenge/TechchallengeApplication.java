dcea662c689e26a0f000cdded7233da8fa0a4d20
Felipe Brandão <felipe.b31@gmail.com>
2023-08-02 13:17:32 -0300

    Configurado swagger

--- src/main/java/com/fiap/techchallenge/TechchallengeApplication.java
+++ src/main/java/com/fiap/techchallenge/TechchallengeApplication.java
@@ -1,9 +1,12 @@
 package com.fiap.techchallenge;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Tech Challenge", version = "0.0.1"))
 public class TechchallengeApplication {
 
 	public static void main(String[] args) {
