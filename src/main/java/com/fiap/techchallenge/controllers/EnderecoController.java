853c510692d0ad62e3d19264ff56f6fd83a1fa08
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-08-27 17:48:10 -0300

    Adicionando os verbos GET e DELETE na classe EnderecoController

--- src/main/java/com/fiap/techchallenge/controllers/EnderecoController.java
+++ src/main/java/com/fiap/techchallenge/controllers/EnderecoController.java
@@ -1,20 +1,22 @@
 package com.fiap.techchallenge.controllers;
 
 import com.fiap.techchallenge.dtos.EnderecoDTO;
+import com.fiap.techchallenge.exceptions.EnderecoNaoEncontradoException;
 import com.fiap.techchallenge.services.EnderecoService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Slf4j
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
@@ -22,13 +24,29 @@ public class EnderecoController {
     @Autowired
     private EnderecoService enderecoService;
 
-    private static final Logger LOGGER = LogManager.getLogger(EnderecoController.class);
+    @GetMapping
+    public ResponseEntity<List<EnderecoDTO>> obterEnderecos() {
+        List<EnderecoDTO> enderecos = enderecoService.obterEnderecos();
+        return ResponseEntity.ok(enderecos);
+    }
 
     @PostMapping
-    public ResponseEntity<EnderecoDTO> cadastro(@Valid @RequestBody EnderecoDTO enderecoDTO, HttpServletRequest request, HttpServletResponse response){
-        LOGGER.info("Inicio da requisição - EnderecoController - cadastro");
+    public ResponseEntity<EnderecoDTO> cadastro(@Valid @RequestBody EnderecoDTO enderecoDTO, HttpServletRequest request, HttpServletResponse response) {
+        log.info("Inicio da requisição - EnderecoController - cadastro");
         EnderecoDTO enderecoGravado = enderecoService.cadastrarEndereco(enderecoDTO);
-        LOGGER.info("Fim da requisição - EnderecoController - cadastro");
+        log.info("Fim da requisição - EnderecoController - cadastro");
         return ResponseEntity.status(HttpStatus.CREATED).body(enderecoGravado);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        log.info("Iniciando exclusão do endereço com ID: " + id);
+        try {
+            enderecoService.deletarEndereco(id);
+            return ResponseEntity.ok("Endereço deletado com sucesso");
+        } catch (EnderecoNaoEncontradoException e) {
+            log.error("Endereço com ID " + id + " não encontrado.");
+            return ResponseEntity.status(NOT_FOUND).body("Endereço não encontrado");
+        }
     }
 }
--- src/main/java/com/fiap/techchallenge/services/EnderecoService.java
+++ src/main/java/com/fiap/techchallenge/services/EnderecoService.java
@@ -2,6 +2,13 @@ package com.fiap.techchallenge.services;
 
 import com.fiap.techchallenge.dtos.EnderecoDTO;
 
+import java.util.List;
+
 public interface EnderecoService {
+
+    List<EnderecoDTO> obterEnderecos();
+
     EnderecoDTO cadastrarEndereco(EnderecoDTO enderecoDTO);
+
+    void deletarEndereco(Long id);
 }
