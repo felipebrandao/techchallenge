bc23cb0796749f9f7104ad4ccd5529194cea26d9
Jonatas Oliveira <jonatas.sp@gmail.com>
2023-09-02 21:28:29 -0300

    CRUD de Eletrodomesticos

---
+++ src/main/java/com/fiap/techchallenge/exceptions/EletrodomesticoCamposNaoPreenchidosException.java
@@ -0,0 +1,5 @@
+package com.fiap.techchallenge.exceptions;
+
+public class EletrodomesticoCamposNaoPreenchidosException extends TechChallengeException {
+    public EletrodomesticoCamposNaoPreenchidosException(String mensagem) { super(mensagem); }
+}
--- src/main/java/com/fiap/techchallenge/mappers/EletrodomesticoMapper.java
+++ src/main/java/com/fiap/techchallenge/mappers/EletrodomesticoMapper.java
@@ -4,8 +4,11 @@ import com.fiap.techchallenge.dtos.EletrodomesticoDTO;
 import com.fiap.techchallenge.entities.Eletrodomestico;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EletrodomesticoMapper {
     Eletrodomestico toEntity(EletrodomesticoDTO eletrodomesticoDTO);
     EletrodomesticoDTO toDTO(Eletrodomestico eletrodomestico);
+    List<EletrodomesticoDTO> toDTOList(List<Eletrodomestico> eletrodomesticos);
 }
