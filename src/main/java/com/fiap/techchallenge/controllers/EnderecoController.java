328f0bdc66a024b9fa7060a2f9ef460a4786b9ae
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-09-04 11:44:37 -0300

    Melhorando a classe EnderecoController

--- src/main/java/com/fiap/techchallenge/controllers/EnderecoController.java
+++ src/main/java/com/fiap/techchallenge/controllers/EnderecoController.java
@@ -3,38 +3,39 @@ package com.fiap.techchallenge.controllers;
 import com.fiap.techchallenge.dtos.EnderecoDTO;
 import com.fiap.techchallenge.exceptions.EnderecoNaoEncontradoException;
 import com.fiap.techchallenge.services.EnderecoService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
 
+    private final EnderecoService enderecoService;
+
     @Autowired
-    private EnderecoService enderecoService;
+    public EnderecoController(EnderecoService enderecoService) {
+        this.enderecoService = enderecoService;
+    }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EnderecoDTO> obterEnderecos(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(enderecoService.getEnderecoById(id));
+    public ResponseEntity<EnderecoDTO> obterEndereco(@PathVariable Long id) {
+        log.info("Obtendo endereço com ID: {}", id);
+        EnderecoDTO endereco = enderecoService.getEnderecoById(id);
+        return ResponseEntity.ok(endereco);
     }
 
     @GetMapping("/buscar")
@@ -42,35 +43,34 @@ public class EnderecoController {
             @RequestParam(required = false) String rua,
             @RequestParam(required = false) String bairro,
             @RequestParam(required = false) String cidade) {
-        return ResponseEntity.status(HttpStatus.OK).body(enderecoService.buscarEnderecoPorFiltro(rua, bairro, cidade));
+        log.info("Buscando endereços por filtro: rua={}, bairro={}, cidade={}", rua, bairro, cidade);
+        List<EnderecoDTO> enderecos = enderecoService.buscarEnderecoPorFiltro(rua, bairro, cidade);
+        return ResponseEntity.ok(enderecos);
     }
 
     @PostMapping
-    public ResponseEntity<EnderecoDTO> cadastro(@Valid @RequestBody EnderecoDTO enderecoDTO, HttpServletRequest request, HttpServletResponse response) {
-        log.info("Inicio da requisição - EnderecoController - cadastro");
-        EnderecoDTO enderecoGravado = enderecoService.cadastrarEndereco(enderecoDTO);
-        log.info("Fim da requisição - EnderecoController - cadastro");
-        return ResponseEntity.status(HttpStatus.CREATED).body(enderecoGravado);
+    public ResponseEntity<EnderecoDTO> cadastrarEndereco(@Valid @RequestBody EnderecoDTO enderecoDTO) {
+        log.info("Cadastrando novo endereço: {}", enderecoDTO);
+        EnderecoDTO enderecoCadastrado = enderecoService.cadastrarEndereco(enderecoDTO);
+        return ResponseEntity.ok(enderecoCadastrado);
     }
 
-    @PutMapping("{id}")
-    public ResponseEntity<EnderecoDTO> atualizarEndereco(@PathVariable Long id, @RequestBody EnderecoDTO enderecoDTO,
-                                                         HttpServletRequest request, HttpServletResponse response) {
-        log.info("Iniciando a atualização da pessoa com ID: {}.", id);
+    @PutMapping("/{id}")
+    public ResponseEntity<EnderecoDTO> atualizarEndereco(@PathVariable Long id, @Valid @RequestBody EnderecoDTO enderecoDTO) {
+        log.info("Atualizando endereço com ID: {}", id);
         EnderecoDTO enderecoAtualizado = enderecoService.atualizarEndereco(id, enderecoDTO);
-        log.info("Pessoa com ID: {} atualizada com sucesso.", id);
-        return ResponseEntity.status(HttpStatus.OK).body(enderecoAtualizado);
+        return ResponseEntity.ok(enderecoAtualizado);
     }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id) {
-        log.info("Iniciando exclusão do endereço com ID: " + id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarEndereco(@PathVariable Long id) {
+        log.info("Excluindo endereço com ID: {}", id);
         try {
             enderecoService.deletarEndereco(id);
-            return ResponseEntity.ok("Endereço deletado com sucesso");
+            return ResponseEntity.noContent().build();
         } catch (EnderecoNaoEncontradoException e) {
-            log.error("Endereço com ID " + id + " não encontrado.");
-            return ResponseEntity.status(NOT_FOUND).body("Endereço não encontrado");
+            log.error("Endereço com ID {} não encontrado.", id);
+            return ResponseEntity.notFound().build();
         }
     }
 }
