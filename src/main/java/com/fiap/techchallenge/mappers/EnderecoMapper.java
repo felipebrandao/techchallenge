47dcbddae00f59b93a07fd4707f39dd5311e27d7
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-09-04 18:52:04 -0300

    Excluido toDTOs não utilizado

--- src/main/java/com/fiap/techchallenge/mappers/EnderecoMapper.java
+++ src/main/java/com/fiap/techchallenge/mappers/EnderecoMapper.java
@@ -4,12 +4,9 @@ import com.fiap.techchallenge.dtos.EnderecoDTO;
 import com.fiap.techchallenge.entities.Endereco;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface EnderecoMapper {
 
     Endereco toEntity(EnderecoDTO enderecoDTO);
     EnderecoDTO toDTO(Endereco endereco);
-    List<EnderecoDTO> toDTOs(List<Endereco> enderecos);
 }
